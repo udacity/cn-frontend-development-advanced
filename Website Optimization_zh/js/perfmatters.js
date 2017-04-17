da661f5aa5e96615de0ebae3b3f768a4c601e2b8
Linesh <linesh.simpcity@gmail.com>
2017-04-17 12:27:22 +0800

    [Linesh][#9] Refactor: rename variable t, dcl and complete

--- Website Optimization_zh/js/perfmatters.js
+++ Website Optimization_zh/js/perfmatters.js
@@ -2,11 +2,11 @@
 // https://developers.google.com/web/fundamentals/performance/critical-rendering-path/measure-crp
 
 function logCRP() {
-  var t = window.performance.timing,
-    dcl = t.domContentLoadedEventStart - t.domLoading,
-    complete = t.domComplete - t.domLoading;
+  var timing = window.performance.timing,
+    domContentLoadedDuration = timing.domContentLoadedEventStart - timing.domLoading,
+    domCompleteDuration = timing.domComplete - timing.domLoading;
   var stats = document.getElementById("crp-stats");
-  stats.textContent = 'DCL: ' + dcl + 'ms, onload: ' + complete + 'ms';
+  stats.textContent = 'DOMContentLoaded: ' + domContentLoadedDuration + 'ms, onload: ' + domCompleteDuration + 'ms';
 }
 
 window.addEventListener("load", function(event) {
